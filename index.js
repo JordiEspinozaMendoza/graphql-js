14009fbf031a7ea7986c6818faf344a32b85bbfb
Jordi <jordi8101@gmail.com>
2022-12-29 10:06:24 -0800

    Authentication and Express Middleware

--- index.js
+++ index.js
@@ -4,58 +4,24 @@ var { buildSchema } = require("graphql");
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-    input MessageInput {
-        content: String
-        author: String
-    }
-    type Message {
-        id: ID!
-        content: String
-        author: String
-    }
     type Query {
-        getMessage(id: ID!): Message
-    }
-    type Mutation {
-        createMessage(input: MessageInput): Message
-        updateMessage(id: ID!, input: MessageInput): Message
+        ip: String
     }
 `);
-// If Message had any complex fields, we'd put them on this object.
-class Message {
-  constructor(id, { content, author }) {
-    this.id = id;
-    this.content = content;
-    this.author = author;
-  }
-}
-// Maps username to content
-var fakeDatabase = {};
-
+const loggingMiddleware = (req, res, next) => {
+  console.log("ip:", req.ip);
+  next();
+};
 // The root provides a resolver function for each API endpoint
 var root = {
-  getMessage: ({ id }) => {
-    if (!fakeDatabase[id]) {
-      throw new Error("no message exists with id " + id);
-    }
-    return new Message(id, fakeDatabase[id]);
-  },
-  createMessage: ({ input }) => {
-    // Create a random id for our "database".
-    var id = require("crypto").randomBytes(10).toString("hex");
-    fakeDatabase[id] = input;
-    return new Message(id, input);
-  },
-  updateMessage: ({ id, input }) => {
-    if (!fakeDatabase[id]) {
-      throw new Error("no message exists with id " + id);
-    }
-    // This replaces all old data, but some apps might want partial update.
-    fakeDatabase[id] = input;
-    return new Message(id, input);
+  ip: function (args, request) {
+    return request.ip;
   },
 };
 var app = express();
+// Use the logging middleware
+app.use(loggingMiddleware);
+// Use the graphql middleware
 app.use(
   "/graphql",
   graphqlHTTP({
